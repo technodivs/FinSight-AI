14e976bc6c49cf62a22211f790ed3e579d41a3b3
Devanshi <devanshisri0805@gmail.com>
2026-07-09 13:37:49 +0530

    fix api url

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,36 +1,17 @@
 import axios from "axios";
 
-
 const API = axios.create({
-
-baseURL:"http://localhost:5000/api"
-
+  baseURL: "https://finsight-ai-backend-xjpp.onrender.com/api",
 });
 
+API.interceptors.request.use((req) => {
+  const token = localStorage.getItem("token");
 
+  if (token) {
+    req.headers.Authorization = `Bearer ${token}`;
+  }
 
-// attach JWT token automatically
-
-API.interceptors.request.use((req)=>{
-
-
-const token =
-localStorage.getItem("token");
-
-
-if(token){
-
-req.headers.Authorization =
-`Bearer ${token}`;
-
-}
-
-
-return req;
-
-
+  return req;
 });
-
-
 
 export default API;
